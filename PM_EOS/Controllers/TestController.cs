65f34654ce303f98f3419e1dbfc18c9d3bdf293b
Phat Trien Soft <devesoft02@hyperlogy.com>
2022-05-09 23:43:48 +0700

    summit code1

---
+++ PM_EOS/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PM_EOS.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
